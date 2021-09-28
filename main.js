f6330d1e5b2ad4514ae8261b8ffe3380b7ad8b41
Raj Bhinde <68704186+irajbhinde@users.noreply.github.com>
2021-09-29 00:31:41 +0530

    fix : Percentage Calculation
    fix : Percentage Calculation

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function calculateProfitOrloss(initial,quantity,current){
     if(initial > current){
         var loss = (initial - current)*quantity
         loss = loss.toFixed(2)
-        var lossPercentage = (loss / initial) * 100
+        var lossPercentage = (loss / (initial*quantity)) * 100
         lossPercentage = lossPercentage.toFixed(2)
         outputDiv.style.color = 'red'
         showMessage(`Oops! You had a loss of ${loss} rupees & ${lossPercentage}%`)
@@ -10,7 +10,7 @@ function calculateProfitOrloss(initial,quantity,current){
     else if (initial < current){
         var profit = (current - initial)*quantity
         profit = profit.toFixed(2)
-        var profitPercentage = (profit / initial)*100
+        var profitPercentage = (profit / (initial*quantity))*100
         profitPercentage = profitPercentage.toFixed(2)
         outputDiv.style.color = 'green'
         showMessage(`Nice! You had a profit of ${profit} rupees & ${profitPercentage}%`);
